215bef5914e20ce1e8737a622bb9d0fdf07bdf68
sbx_user1051 <sbx_user1051@169.254.59.149>
2022-04-07 20:19:48 +0000

    Thu Apr 07 20:19:48 UTC 2022

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -6,17 +6,19 @@ public class CheckoutSolution {
     public boolean isvalid(String str){
         if (str == null)
             return false;
-        for(){
-            if(s != "A" && s != "B" && s != "C" && s != "D")
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) != 'A' && str.charAt(i) != 'B' && str.charAt(i) != 'C' && str.charAt(i) != 'D')
                 return false;
         }
         return true;
     }
 
     public Integer checkout(String skus) {
-
+        if(isvalid(skus) == false)
+            return 
 
     }
 }
+
 
 
