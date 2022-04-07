d8260067d80583ae4688eeff7688597efd4c7224
sbx_user1051 <sbx_user1051@169.254.59.149>
2022-04-07 20:13:48 +0000

    Thu Apr 07 20:13:48 UTC 2022

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -3,7 +3,15 @@ package befaster.solutions.CHK;
 import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
+    public boolean isvalid(String str){
+        for(char s in str){
+            
+        }
+    }
+
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+
+
     }
 }
+
