fa30f1ff663c2162446815698ba1de19734aab21
sbx_user1051 <sbx_user1051@169.254.59.149>
2022-04-07 20:28:48 +0000

    Thu Apr 07 20:28:48 UTC 2022

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -13,12 +13,46 @@ public class CheckoutSolution {
         return true;
     }
 
-    public Integer checkout(String skus) {
-        if(isvalid(skus) == false)
-            return 
+    public Integer totalchar(String str, char c){
+        Integer counter = 0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == c )
+                counter++;
+        }
+        return counter;
+    }
 
+    public Integer checkout(String skus) {
+        if(!isvalid(skus))
+            return -1;
+        Integer totalprice = 0;
+
+        Integer totalA = totalchar(skus,'A');
+
+        while(totalA > 3){
+            totalprice +=130;
+            totalA-=3;
+        }
+        totalprice += totalA*50;
+
+        Integer totalB = totalchar(skus,'B');
+
+        while(totalB > 3){
+            totalprice +=45;
+            totalB-=3;
+        }
+        totalprice +=  totalB*30;
+
+        Integer totalC = totalchar(skus,'C');
+        totalprice += totalC*20;
+
+        Integer totalD = totalchar(skus,'D');
+        totalprice += totalD*15;
+
+        return totalprice;
     }
 }
+
 
 
 
---
+++ src/test/java/befaster/solutions/market/MarketTest.java
@@ -0,0 +1,33 @@
+package befaster.solutions.market;
+
+import befaster.solutions.CHK.CheckoutSolution;
+import befaster.solutions.SUM.SumSolution;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+
+        import org.junit.Before;
+        import org.junit.Test;
+
+        import static org.hamcrest.MatcherAssert.assertThat;
+        import static org.hamcrest.Matchers.equalTo;
+
+public class MarketTest {
+    private CheckoutSolution market;
+
+    @Before
+    public void setUp() {
+
+        market = new CheckoutSolution();
+    }
+
+    @Test
+    public void markettest() {
+        assertThat(market.checkout("ABCD"), equalTo(115));
+        assertThat(market.checkout("AAABCD"), equalTo(195));
+    }
+}
+
